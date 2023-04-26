11e4665f1c2b1520ed366f041cf58d92f211e8c0
Johann Dreo <johann.dreo@pasteur.fr>
2023-04-26 13:23:31 +0200

    simple working test

--- make.py
+++ make.py
@@ -1,22 +1,29 @@
+#!/usr/bin/env python3
+
 from biocypher import BioCypher
 
-import oncodashkb
+import oncodashkb.adapters as od
 
 if __name__ == "__main__":
 
     bc = BioCypher(biocypher_config_path = "config/biocypher_config.yaml")
 
-    allowed ={
-        "node_types"  = [ ],
-        "node_fields" = [ ],
-        "edge_types"  = [ ],
-    }
+    allowed_node_types  = [ od.types.Patient ]
+    allowed_node_fields = [ ]
+    allowed_edge_types  = [ ]
 
-    oncokb = OncoKB(*allowed)
+    oncokb = od.oncokb.OncoKB(
+        allowed_node_types,
+        allowed_node_fields,
+        allowed_edge_types
+    )
 
-    bc.write_nodes( oncokb.nodes() )
-    bc.write_edges( oncokb.edges() )
+    for n in oncokb.nodes():
+        print(n)
 
-    bc.write_import_call()
+    # bc.write_nodes( oncokb.nodes() )
+    # bc.write_edges( oncokb.edges() )
 
-    bc.summary()
+    # bc.write_import_call()
+
+    # bc.summary()
