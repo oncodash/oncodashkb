641e7fce2bf00ec1b16094d8ac002f60fb23a94f
Johann Dreo <johann.dreo@pasteur.fr>
2024-09-12 15:22:39 +0200

    update(make.sh): add all data sources, follow the new dir convention

--- make.sh
+++ make.sh
@@ -9,13 +9,10 @@ NEO_USER="sudo -u neo4j"
 
 # Exit on error.
 set -e
+set -o pipefail
 
-if [[ $# -ne 1 ]]; then
-    printf "Usage: %s <data_dir>\n" "$0"
-    exit 2 # usage
-fi
-
-data_dir="$1"
+# data_dir="$1"
+data_dir="data"
 
 source $(poetry env info --path)/bin/activate
 
@@ -29,11 +26,21 @@ else
     server="${NEO_USER} neo4j-admin server"
 fi
 $server stop
-# ./weave.py --oncokb $data_dir/genomics_oncokbannotation.csv --cgi $data_dir/genomics_cgimutation.csv --clinical $data_dir/clin_overview_clinicaldata.csv > tmp.sh
-./weave.py --oncokb $data_dir/genomics_oncokbannotation.csv > tmp.sh
+./weave.py --verbose DEBUG \
+    --open_targets $data_dir/OT/targets \
+    --open_targets_drugs $data_dir/OT/molecule \
+    --open_targets_diseases $data_dir/OT/diseases \
+    --open_targets_evidences $data_dir/OT/evidence/sourceId\=chembl \
+    --oncokb $data_dir/DECDIER/genomics_oncokbannotation.csv \
+    --cgi $data_dir/DECDIER/genomics_cgimutation.csv \
+    --gene_ontology_owl $data_dir/GO/go.owl \
+    --gene_ontology_genes $data_dir/DECIDER/Hugo_Symbol_genes.conf \
+    --gene_ontology $data_dir/GO/goa_human.gaf \
+    --clinical $data_dir/DECDIER/clin_overview_clinicaldata.csv \
+    > tmp.sh
+
 cat $(cat tmp.sh) | tee /dev/tty | ${NEO_USER} sh
 $server start
 sleep 5
-# cypher-shell --username neo4j --database oncodash --password $(cat neo4j.pass) "MATCH (n) RETURN n;"
-cypher-shell --username neo4j --database oncodash --password $(cat neo4j.pass) "MATCH (p:Patient) RETURN p;"
+cypher-shell --username neo4j --database oncodash --password $(cat neo4j.pass) "MATCH (p:Patient) RETURN p LIMIT 20;"
 
