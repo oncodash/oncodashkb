6bfc4d94b937c5ac859553110ff1ae9f8e4b985d
Marko Baric <marko.baric@pasteur.fr>
2024-11-15 16:59:16 +0100

    fix(adapters):Remove redundant adapters from init
    -cgi, oncokb, clinical

--- oncodashkb/adapters/__init__.py
+++ oncodashkb/adapters/__init__.py
@@ -1,12 +1,9 @@
 
 # from . import types
-from . import oncokb
-from . import cgi
-from . import clinical
 from . import gene_ontology
 from . import open_targets
 from . import open_targets_evidences
 from . import open_targets_drugs
 from . import open_targets_diseases
-__all__ = ['types', 'oncokb', 'cgi', 'clinical', 'gene_ontology', 'open_targets', 'open_targets_evidences', 'open_targets_drugs', 'open_targets_diseases']
+__all__ = ['types', 'gene_ontology', 'open_targets', 'open_targets_evidences', 'open_targets_drugs', 'open_targets_diseases']
 
