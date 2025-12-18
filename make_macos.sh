bd03cc529c8ee81dd737c956c0cabf1306c009cc
Matthieu NAJM <matthieu.najm@pasteur.fr>
2025-12-18 15:36:35 +0100

    fix: fix make.sf for MacOS

--- make_macos.sh
+++ make_macos.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/bash
 
-# When using Neo4j installed on system (like Ubuntu's packaged version),
-# the current directory must be writable by user "neo4j",
-# and all parent directories must be executable by "other".
-# Every interaction with the database must be done by user "neo4j",
-# and the import will try to write reports in the current directory.
-# NEO_USER="neo4j"
-
 # Exit on error.
 set -e
 set -o pipefail
-
-echo "$1 and $2"
 
 data_dir="data"
 data_version="$1"
@@ -23,9 +14,6 @@ if [[ "$2" == "debug" ]] ; then
     py_args=""
     weave_args="-v DEBUG"
 fi
-
-# export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-amd64"
-
 
 echo "Activate virtual environment..." >&2
 source $(poetry env info --path)/bin/activate
@@ -45,9 +33,11 @@ fi
 echo "Weave data..." >&2
 
 cmd="python3 ${py_args} $(pwd)/weave.py --verbose INFO \
-    --clinical                   $data_dir/DECIDER/$data_version/clinical_export.csv \
-    --short_mutations_external   $data_dir/DECIDER/$data_version/snv_external.csv  \
-    --short_mutations_local      $data_dir/DECIDER/$data_version/snv_local.csv \
+    --clinical                              $data_dir/DECIDER/$data_version/clinical_export.csv \
+    --short_mutations_local                 $data_dir/DECIDER/$data_version/snv_local.csv \
+    --short_mutations_external              $data_dir/DECIDER/$data_version/snv_external.csv  \
+    --copy_number_amplifications_local      $data_dir/DECIDER/$data_version/cna_local.csv \
+    --copy_number_amplifications_external   $data_dir/DECIDER/$data_version/cna_external.csv  \
     ${weave_args}" # \
 
 echo "Weaving command:" >&2
@@ -56,20 +46,17 @@ echo "$cmd" >&2
 $cmd > tmp.sh
 
 
-# if [[ "$2" != "debug" ]] ; then
-#     echo "Run import script..." >&2
-#     chmod a+x  $(cat tmp.sh)
-#     $(cat tmp.sh) | tee /dev/tty | ${NEO_USER} sh
+if [[ "$2" != "debug" ]] ; then
+    echo "Run import script..." >&2
+    chmod a+x  $(cat tmp.sh)
+    sh $(cat tmp.sh)
 
-#     echo "Restart Neo4j..." >&2
-#     $server start
-#     sleep 5
+    echo "Restart Neo4j..." >&2
+    $server start
+    sleep 5
 
-#     echo "Send a test query..." >&2
-#     sudo -u neo4j cypher-shell --username neo4j --database oncodash --password $(cat neo4j.pass) "MATCH (p:Patient) RETURN p LIMIT 20;"
-# fi
+    echo "Send a test query..." >&2
+    cypher-shell --username neo4j --database oncodash --password $(cat neo4j.pass) "MATCH (p:Patient) RETURN p LIMIT 20;"
+fi
 
-# echo "Done" >&2
-
-# H123
-
+echo "Done" >&2
